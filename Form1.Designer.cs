755ec5042890acd23c178bda7b6f8b0ad15610c8
KonstantinaAndr <andreadikonstan@gmail.com>
2017-05-08 18:11:13 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.SlateGray;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "City";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(12, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "°C      °F";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 187);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(263, 115);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::weather.Properties.Resources.weather;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(564, 325);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "weather";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace weather
+{
+    public partial class Form1 : Form
+           
+    {       
+            public Form1()
+        {
+            InitializeComponent();
+        }
+        private const string API_KEY = "e1053d1b85007fa1f764226b8401b627";
+
+        private const string CurrentUrl =
+           "http://api.openweathermap.org/data/2.5/weather?" +
+           "q=@LOC@&mode=xml&units=imperial&APPID=" + API_KEY;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = CurrentUrl.Replace("@LOC@", textBox1.Text);
+                richTextBox1.Text = GetFormattedXml(url);
+                XmlDocument xml_doc = new XmlDocument();
+                string weburl = "http://api.openweathermap.org/data/2.5/weather?q=" + textBox1.Text + "&mode=xml";
+                var xml = new WebClient().DownloadStringTaskAsync(new Uri(weburl));
+                string szTemp = xml_doc.DocumentElement.SelectSingleNode("temperature").Attributes["value"].Value;
+                double temp = double.Parse(szTemp) - 273.16;
+                richTextBox1.Text = temp.ToString("N2") + " Celcius";
+            }
+            catch(Exception)
+            { }  
+        }
+        private string GetFormattedXml(string sztemp)
+       {
+           using (WebClient client = new WebClient())
+           {
+               string xml = client.DownloadString(CurrentUrl);
+               XmlDocument xml_doc = new XmlDocument();
+               xml_doc.LoadXml(xml);
+              XmlNodeList xmlnd = xml_doc.SelectNodes("//temperature");
+                                
+               using (StringWriter stringwrt = new StringWriter())
+              {
+                 XmlTextWriter xmltextwrit = new XmlTextWriter(stringwrt);
+                  xmltextwrit.Formatting = Formatting.Indented;
+                  xml_doc.WriteTo(xmltextwrit);
+               return stringwrt.ToString();
+              }
+            }
+        }
+       
+    
+  
